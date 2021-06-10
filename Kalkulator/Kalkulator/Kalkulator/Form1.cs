ddac0ee572a8712e4f97659e87bdbb788909fad6
sudo555 <84177901+sudo555@users.noreply.github.com>
2021-06-10 16:28:46 +0200

    Add files via upload

---
+++ Kalkulator/Kalkulator/Kalkulator/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        string equation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            equation += "1";
+            textBox1.Text = equation;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            equation += "2";
+            textBox1.Text = equation;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            equation += "3";
+            textBox1.Text = equation;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            equation += "4";
+            textBox1.Text = equation;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            equation += "5";
+            textBox1.Text = equation;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            equation += "6";
+            textBox1.Text = equation;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            equation += "7";
+            textBox1.Text = equation;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            equation += "8";
+            textBox1.Text = equation;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            equation += "9";
+            textBox1.Text = equation;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            equation += "0";
+            textBox1.Text = equation;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if(equation != "" )
+            {
+                equation += ".";
+                textBox1.Text = equation;
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            equation += "+";
+            textBox1.Text = equation;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            equation += "-";
+            textBox1.Text = equation;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (equation != "")
+            {
+                equation += "*";
+                textBox1.Text = equation;
+            }
+           
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (equation != "")
+            {
+                equation += "/";
+                textBox1.Text = equation;
+            }
+        }
+
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            equation = "";
+            textBox1.Text = equation;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = Convert.ToDouble(new DataTable().Compute(equation, null));
+                if (double.IsFinite(result))
+                {
+                    equation = result.ToString();
+                    textBox1.Text = equation;
+                }
+                else
+                {
+                    textBox1.Text = "Błąd";
+                }
+            }
+            catch
+            {
+                textBox1.Text = "Błąd";
+            }
+            
+          
+            
+        }
+    }
+}
---
+++ Matematyka/Matematyka/Matematyka/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matematyka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y, z, sum;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z = Convert.ToDouble(textBox3.Text);
+            sum = x + y + z;
+            textBox4.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x, y, z, product;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z = Convert.ToDouble(textBox3.Text);
+            product = x * y * z;
+            textBox4.Text = product.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if( !int.TryParse(textBox1.Text, out _)) // Sprawdza czy string nie jest liczbą
+            {
+                if( textBox1.Text.Length > 0)
+                {
+                    textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                }
+                else
+                {
+                    textBox1.Text = "";
+                }
+                
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox2.Text, out _)) // Sprawdza czy string nie jest liczbą
+            {
+                if (textBox2.Text.Length > 0)
+                {
+                    textBox2.Text = textBox2.Text.Substring(0, textBox2.Text.Length - 1);
+                    textBox2.SelectionStart = textBox2.Text.Length;
+                }
+                else
+                {
+                    textBox2.Text = "";
+                }
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox3.Text, out _)) // Sprawdza czy string nie jest liczbą
+            {
+                if (textBox3.Text.Length > 0)
+                {
+                    textBox3.Text = textBox3.Text.Substring(0, textBox3.Text.Length - 1);
+                    textBox3.SelectionStart = textBox3.Text.Length;
+                }
+                else
+                {
+                    textBox3.Text = "";
+                }
+
+            }
+        }
+    }
+}
